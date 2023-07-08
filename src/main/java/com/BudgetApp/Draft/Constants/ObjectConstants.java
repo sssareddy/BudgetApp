fd3a5b3d88b8404d189498cc30081e17a8be85c6
ayyap <ayyap@Saransurekha>
2023-07-08 20:15:25 +0530

    Removed Personal keys

--- src/main/java/com/BudgetApp/Draft/Constants/ObjectConstants.java
+++ src/main/java/com/BudgetApp/Draft/Constants/ObjectConstants.java
@@ -13,8 +13,8 @@ public class ObjectConstants {
 	public static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
 	public static final String CREDENTIALS_FILE_PATH = "src/main/resources/config.properties";
 	public static final String APPLICATION_NAME = "Spring Boot Budget App";
-	public static final String spreadsheetId = "1s-4f_7plTvDmdPxIIi5H99edv_fQleL22aYpgGPpSws";
-	public static final String spreadsheetId_Perticuler="1N8fkHKJ-Qr84Qwpqt2dMs7iM6J-EQB_9KVRe63G_tA8";
+	public static final String spreadsheetId = "";
+	public static final String spreadsheetId_Perticuler="";
 	public static final String range = "A:F";
 	public static final String readRange="June!A:F";
 	public static final String readRangePerticular="A";
@@ -24,7 +24,7 @@ public class ObjectConstants {
 	public static final String itemSuccessResponse="Item Added Successfully";
 	public static final String dataPattren="dd/MMM/yyyy";
 	public static final String access_token_string="access_token";
-	public static final String TOKEN_URI="https://oauth2.googleapis.com/token?client_secret=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&grant_type=refresh_token&refresh_token=1//04gBnFGRjRDU6CgYIARAAGAQSNwF-L9Irpb3ccPv6yq-R0bcBoI7JaCxgiEcs2BrFNK4NuE24feet3FbD0Cd7PN2cpjGMmRS9Hs0&client_id=516952147863-1t8o644id53n7vsiblmtcbm9lb7s1f7v.apps.googleusercontent.com";
+	public static final String TOKEN_URI="";
 	public static final String VALIDATE_TOKEN_URI="https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=";
 	public static final String VALIDATE_TOKEN_ERROR="Error occurred while checking the access token validity: ";
 	public static final String CONTENT_TYPE="Content-Type";
